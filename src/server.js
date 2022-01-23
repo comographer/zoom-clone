df34ec98352e296b8a1ac2af6ccf694adf423a4c
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-23 21:16:58 +0100

    Finish Setup

--- src/server.js
+++ src/server.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-console.log("Hello World");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(PORT);
+const handleListening = () =>
+  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
