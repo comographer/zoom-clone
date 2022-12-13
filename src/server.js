d0c483064f969d69ef33eb437776ce372d8bee35
Geunmo Kim <kimgeunmo@gmail.com>
2022-12-13 20:31:14 +0100

    1.2 WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+import http from "http";
 import express from "express";
+import WebSocket from "ws";
 
 const app = express();
 const PORT = 3000;
@@ -10,10 +12,23 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening to http://localhost:${PORT}`);
-app.listen(PORT, handleListen);
+// app.listen(PORT, handleListen);
+
+// Gain access to the server
+const server = http.createServer(app);
+
+// Handling http server and ws server together on same port
+const wss = new WebSocket.Server({ server });
+
+server.listen(PORT, handleListen);
 
 /* 
 1.1 HTTP vs WebSockets
 - HTTP works on 1 req, 1 res basis.
 - WebSockets works on connection where multiple actions can happen as long as connection is there.
+*/
+
+/* 
+1.2 WebSockets in NodeJS
+- 
 */
