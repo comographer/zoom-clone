c2b750f1405ddf0be51b1cec308f577a9544362e
Geunmo Kim <kimgeunmo@gmail.com>
2022-12-14 23:24:08 +0100

    1.3 WebSocket Events

--- src/server.js
+++ src/server.js
@@ -20,15 +20,16 @@ const server = http.createServer(app);
 // Handling http server and ws server together on same port
 const wss = new WebSocket.Server({ server });
 
+function handleConnection(socket) {
+  console.log(socket);
+}
+
+wss.on("connection", handleConnection);
+
 server.listen(PORT, handleListen);
 
 /* 
 1.1 HTTP vs WebSockets
 - HTTP works on 1 req, 1 res basis.
 - WebSockets works on connection where multiple actions can happen as long as connection is there.
-*/
-
-/* 
-1.2 WebSockets in NodeJS
-- 
 */
